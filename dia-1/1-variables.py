e59fb6fb41723c4d864f4be6091a10ec25a2186a
LenerWeb <lener20@hotmail.com>
2022-10-10 21:23:25 -0500

    agregacion: agregue los archivos iniciales de python

---
+++ dia-1/1-variables.py
@@ -0,0 +1,34 @@
+# Esto es un comentario
+edad = 30 
+
+nombre = 'Eduardo'
+apellido = "De Rivero"
+
+mensaje = '''El dia de hoy
+empezo el modulo de 
+backend'''
+
+despedida = """El dia de hoy
+nos despedimos hasta una nueva
+oportunidad"""
+
+LastName = "O'neil"
+
+print(nombre)
+especialidad = None
+
+LastName = "80"
+LastName = "None"
+
+print(type(nombre))
+print(type(edad))
+
+#tambien se puede declarar varias variables en una misma linea
+curso, grupo, mes, dia, nota = 'CodiGo','Backend', 'Octubre', 10, 15.4
+
+print(dia)
+
+print(id(curso))
+del curso
+
+print(curso)
---
+++ dia-1/2-impresion-pantalla.py
@@ -0,0 +1,14 @@
+correo = 'lener20@hotmail.com'
+estatura = 1.65
+print(correo)
+
+#juntar dos textos, al momento de poner la coma se agrega un espacio al imprimir
+print('el correo del usuario es',correo)
+
+print('el correo del usuario es '+correo)
+
+#concatenacion por el mas (+)solo se hace entre string
+print('la estatura del usuario es ',estatura)
+
+#parsear > convertir un tipo de dato a otro
+print('la estatura del usuario es '+str(estatura))
