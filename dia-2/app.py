68e56c89e49d38bbb9a1f6a12cf4889991800410
LenerWeb <lener20@hotmail.com>
2022-11-03 23:05:29 -0500

    traer tabla jinja

--- dia-2/app.py
+++ dia-2/app.py
@@ -16,7 +16,7 @@ app.config['MYSQL_PASSWORD']= environ.get('MYSQL_PASSWORD')
 app.config['MYSQL_DB']= environ.get('MYSQL_DB')
 app.config['MYSQL_PORT']= int(environ.get('MYSQL_PORT'))
 
-print(app.config) 
+# print(app.config) 
 mysql = MySQL(app)
 
 # un decorador es la forma en la cual nosotros podemos modificar el comportamiento de un metodo de una clase sin la necesidad de modificarlo directamente, es como utilizar la herencia para poder modificar su comportamiento, en este casdo, dependiendo de su ruta y su metodo
@@ -30,7 +30,7 @@ def inicio():
 def pagina_inicial():
     return render_template('inicio.html') 
 
-@app.route('/alumnos', methods=['GET'])
+@app.route('/mostrar-alumnos', methods=['GET'])
 def devolver_alumnos():
     #me crea una conexion con la base de datos
     cursor = mysql.connection.cursor() 
@@ -39,10 +39,30 @@ def devolver_alumnos():
     # devolver toda la informacion de esa consulta
     resultado = cursor.fetchall()
     print(resultado)
+    resultado_final = []
+    #itero mi resultado de mi consulta a la bd
+    for alumno in resultado:
+        # creo un diccionario para indicar la llave de cada elemento
+        alumno_diccionario = {
+            'id':alumno[0],
+            'nombre':alumno[1],
+            'ape_paterno':alumno[2],
+            'ape_materno':alumno[3],
+            'correo':alumno[4],
+            'num_emergencia':alumno[5],
+            'curso_id':alumno[6]
+        }
+        print(alumno_diccionario)
+        resultado_final.append(alumno_diccionario)
+    #return {
+    #    'message':'Los alumnos son:',
+    #    'content': resultado_final
+    #}
+    return render_template('mostrar_alumnos.html', alumnos=resultado_final, mensaje='Hola desde Flask')
 
-    return {
-        'message':'Los alumnos son:'
-    }
+@app.route("/agregar-alumno", methods = ['GET'])
+def agregar_alumno():
+    return render_template ('agregar_alumno.html')
 
 app.run(debug=True)
 
