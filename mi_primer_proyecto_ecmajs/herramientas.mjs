59529bd59090d2e8a1c1da2732800b003c8a8751
LenerWeb <lener20@hotmail.com>
2022-12-01 21:04:38 -0500

    proyecto usando CommonScript y ECMAscript

---
+++ mi_primer_proyecto_ecmajs/herramientas.mjs
@@ -0,0 +1,8 @@
+//cuando nosotros queremos utilizar el formato de ecmascrip, la forma en la que node reconoce que utilizaremos ese forma de trabajar sera mendiante la extension del archivo
+
+export const edad = 30;
+export const nacionalidad = 'Peruano';
+
+export function saludar(nombre) {
+    return nombre;
+}
