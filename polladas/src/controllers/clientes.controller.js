b79564b4429fbb253ddbde877be66e4de629ad4d
LenerWeb <lener20@hotmail.com>
2022-12-13 23:43:47 -0500

    final de proyecto polladas

--- polladas/src/controllers/clientes.controller.js
+++ polladas/src/controllers/clientes.controller.js
@@ -1,15 +1,15 @@
-import { Prisma } from "../prisma";
+import { Prisma } from "../prisma.js";
 
 export const crearCliente = async(req, res)=>{
     const data = req.body
     try {
         const cliente = await Prisma.cliente.create({
-            data,
+            data
         });
         return res.status(201).json({
             message: "Cliente creado",
             content: cliente,
-        })
+        });
     } catch (error) {
         return res.status(500).json({
             message: "Error en el servidor",
@@ -17,3 +17,113 @@ export const crearCliente = async(req, res)=>{
         });
     }
 };
+
+export const listarClientes = async(req, res) => {
+    try {
+        const clientes = await Prisma.cliente.findMany()
+        return res.status(200).json({
+            message:"Lista clientes",
+            content: clientes
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error inesperado",
+            error: error.message,
+        })
+    }
+}
+
+export const traerClientePorId = async(req, res) => {
+    const { id } = req.params;
+    try {
+        const cliente = await Prisma.cliente.findUnique({
+            where: {
+                id: Number(id),
+            },
+        });
+        if (!cliente){
+            return res.status(404).json({
+                message:"Cliente no encontrado",
+            });
+        }
+        return res.status(200).json({
+            message: "Cliente encontrado",
+            content: cliente,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error inesperado",
+            error: error.message,
+        });
+    }
+}
+
+export const actualizarCliente = async(req, res) => {
+    const { id } = req.params;
+    const data = req.body;
+    try {
+        const findCliente = await Prisma.cliente.findUnique({
+            where: {
+                id: Number(id),
+            },
+        });
+        if (!findCliente){
+            return res.status(404).json({
+                message:"Cliente no encontrado",
+            });
+        };
+
+        const cliente = await Prisma.cliente.update({
+            where: { 
+                id: Number(id),
+            },
+            data: {
+                nombre: data.nombre,
+                apellido: data.apellido,
+                dni: data.dni,
+                correo: data.correo,
+            },
+        })
+        return res.status(201).json({
+            message:"Cliente actualizado",
+            content: cliente,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error inesperado",
+            error: error.message,
+        });
+    }
+}
+
+export const eliminarCliente = async(req, res) => {
+    const { id } = req.params;
+    try {
+        const findCliente = await Prisma.cliente.findUnique({ // puede ser findfirst
+            where:  { 
+                id: Number(id),
+            },
+        });
+        if (!findCliente) {
+            return res.status(404).json({
+                message: "Cliente no encontrado"
+            });
+        };
+        const cliente = await Prisma.cliente.delete({
+            where: {
+                id: Number(id),
+            },
+        });
+        return res.status(200).json({
+            message: "Cliente eliminado",
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error inesperado",
+            error: error.message,
+        });
+    }
+}
